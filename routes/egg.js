6c41889bbfcbc7ebc4ddd6ffd9c4beb63d76f596
nannehussain <143141256+nannehussain@users.noreply.github.com>
2023-11-01 22:14:37 -0500

    last commit done

--- routes/egg.js
+++ routes/egg.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('egg', { title: 'Search Results egg' });
 });
-
 module.exports = router;
